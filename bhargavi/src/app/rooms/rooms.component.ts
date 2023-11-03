b474a668989da0f619213b12cad2eb75478b4bb4
93bhargavi <137365939+93bhargavi@users.noreply.github.com>
2023-11-03 22:40:31 +0530

    updated

--- bhargavi/src/app/rooms/rooms.component.ts
+++ bhargavi/src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Room, RoomsList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -6,20 +7,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rooms.component.css']
 })
 export class RoomsComponent implements OnInit {
-  hotelName="Bhargavi";
+  hotelName="Kushi Hotel";
   numberOfRooms=10;
-
   hideRooms=false;
 
 
+  rooms : Room =
+   {
+    totalRooms:20,
+    availableRooms:10,
+    bookbedRooms: 5
+  }
+
+  roomList : RoomsList[] = [
+  {
+    roomNumber:1,
+    roomType :  'Deluxe Room',
+    amenities:  'Air Conditioner,Free wifi,Tv,Bathroom,Kitchen',
+    price:1000,
+    photos:
+    'src=https://media.istockphoto.com/id/457982209/photo/t…20&c=vweeII_P5daI0zUzwLZPVgi4zCGrOV2BKJY-LpYN4qI=',
+    checkinTime: new Date('28-Oct-2023'),
+    checkoutTime:new Date('30-Oct-2023'),
+    rating:4,
+  },
+
+  {
+    roomNumber:2,
+    roomType :  'Private Room',
+    amenities:  'Air Conditioner,Free wifi,Tv,Bathroom,Kitchen,soap,shampoo,hotwater',
+    price:2000,
+    photos:
+    'src=https://media.istockphoto.com/id/457982209/photo/t…20&c=vweeII_P5daI0zUzwLZPVgi4zCGrOV2BKJY-LpYN4qI=',
+    checkinTime: new Date('28-Oct-2023'),
+    checkoutTime:new Date('30-Oct-2023'),
+    rating:4.5,
+
+  },
+
+  {
+    roomNumber:3,
+    roomType :  'Luxury Room',
+    amenities:  'Air Conditioner,Free wifi,Tv,Bathroom,Kitchen,soap,shampoo,hotwater,doublebed,swimmingpool',
+    price:3000,
+    photos:
+    'src=https://media.istockphoto.com/id/457982209/photo/t…20&c=vweeII_P5daI0zUzwLZPVgi4zCGrOV2BKJY-LpYN4qI=',
+    checkinTime: new Date('28-Oct-2023'),
+    checkoutTime:new Date('30-Oct-2023'),
+    rating:5,
+
+  },
+
+  {
+    roomNumber:4,
+    roomType :  'Luxury Room',
+    amenities:  'Air Conditioner,Free wifi,Tv,Bathroom,Kitchen,soap,shampoo,hotwater,doublebed,swimmingpool,cook',
+    price:4000,
+    photos:
+    'src=https://media.istockphoto.com/id/457982209/photo/t…20&c=vweeII_P5daI0zUzwLZPVgi4zCGrOV2BKJY-LpYN4qI=',
+    checkinTime: new Date('28-Oct-2023'),
+    checkoutTime:new Date('30-Oct-2023'),
+    rating:4.8,
+
+  }
+
+]
+
   constructor(){}
 
-   
   ngOnInit():void{ }
 
-  toggle(){
-    this.hideRooms =!this.hideRooms;
-  }
+  toggle(){this.hideRooms =!this.hideRooms;}
+
 
 }
 
---
+++ bhargavi/src/app/rooms/rooms.ts
@@ -0,0 +1,16 @@
+export interface Room{
+    totalRooms:number;
+    availableRooms:number;
+    bookbedRooms:number;
+}
+
+export interface RoomsList{
+    roomNumber:any;
+    roomType : any;
+    amenities: any;
+    price: any;
+    photos : any;
+    checkinTime: any;
+    checkoutTime: any;
+    rating:any;
+}
